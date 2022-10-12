b2c4d10a0caa127af3c5d81c9a674e0e7c7173e3
Vinicius Cipolli - CPL <viniciuscipolli@gmail.com>
2022-10-12 19:12:09 -0300

    Teste

--- src/main/kotlin/com/cipolli/port/input/HelloMicronaut.kt
+++ src/main/kotlin/com/cipolli/port/input/HelloMicronaut.kt
@@ -3,6 +3,7 @@ package com.cipolli.port.input
 import io.micronaut.http.MediaType
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
+import io.micronaut.http.annotation.Post
 import io.micronaut.http.annotation.Produces
 
 @Controller("/hello")
@@ -13,5 +14,11 @@ class HelloMicronaut {
     @Produces(MediaType.TEXT_PLAIN)
     public fun hello(): String {
         return "Hello Micronauter!!!";
+    }
+
+    @Post
+    @Produces(MediaType.TEXT_PLAIN)
+    public fun helloPost(saveThis: String): String {
+        return "Hello Post Micronauter!!!";
     }
 }
